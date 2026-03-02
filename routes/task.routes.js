38be62b9314df7ca5457ed7e1040fa42851dc453
VimalnathJ <vimalnathj64@gmail.com>
2026-03-02 13:48:29 +0530

    fix : url error

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -4,8 +4,8 @@ const taskController = require("../controllers/task.controller");
 
 router.post("/api/createtasks", taskController.createTask);
 router.get("/api/gettasks", taskController.getAllTasks);
-router.get("/api/singletasks:id", taskController.getSingleTask);
-router.put("/api/updatetasks:id", taskController.updateTask);
+router.get("/api/singletasks/:id", taskController.getSingleTask);
+router.put("/api/updatetasks/:id", taskController.updateTask);
 router.delete("/api/deletetasks:id", taskController.deleteTask);
 
 module.exports = router;
