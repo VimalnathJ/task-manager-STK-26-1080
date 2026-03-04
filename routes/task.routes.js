ab0d5dbb5adf7f254a44c15b8afd1b5684886c09
VimalnathJ <vimalnathj64@gmail.com>
2026-03-04 13:12:59 +0530

    feat : added pagination to router

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -5,6 +5,7 @@ const protect = require("../middlewares/auth.middleware");
 
 router.post("/api/createtasks", protect, taskController.createTask);
 router.get("/api/gettasks", protect, taskController.getAllTasks);
+router.get("/api/getbypages", protect, taskController.getPages);
 router.get("/api/singletasks/:id", protect, taskController.getSingleTask);
 router.put("/api/updatetasks/:id", protect, taskController.updateTask);
 router.delete("/api/deletetasks/:id", protect, taskController.deleteTask);
