164e7b0e4583c8a2c1a526c5e4f3aeeb610e08c2
VimalnathJ <vimalnathj64@gmail.com>
2026-03-04 13:35:06 +0530

    feat : Added default limit 10

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -26,7 +26,7 @@ exports.getAllTasks = async (req, res) => {
 exports.getPages = async (req, res) => {
     try{
         const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit);
+        const limit = parseInt(req.query.limit) || 10;
         const skip = (page - 1) * limit;
         const totalTasks = await Task.countDocuments();
 
